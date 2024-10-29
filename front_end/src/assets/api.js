151f4e49d093f63efe1105813b1a3477c020c5ba
Nelson <nelsonba787@gmail.com>
2024-10-29 18:07:27 -0400

    Refactor API base URL to use production backend URL

--- front_end/src/assets/api.js
+++ front_end/src/assets/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Configuración base de Axios
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000", // Reemplaza con la URL de tu backend
+  baseURL: "https://wrkr.onrender.com", // Reemplaza con la URL de tu backend
   headers: {
     "Content-Type": "application/json",
   },
