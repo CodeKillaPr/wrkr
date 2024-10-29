0cd9ac59d2b4b4ac7279313d1198cb38da275caa
Nelson <nelsonba787@gmail.com>
2024-10-29 18:43:37 -0400

    Refactor asset URLs to use relative paths and update API base URL to production backend URL

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -2,12 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",
+  base: "/wrkr/front_end/",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
   server: {
     proxy: {
       "/api": {
