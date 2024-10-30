e2aa8028d7091466077fc1362f398004917fc8b2
Nelson <nelsonba787@gmail.com>
2024-10-29 20:02:44 -0400

    Refactor base path in vite.config.js

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/main.jsx/",
+  base: "./",
   plugins: [react()],
   server: {
     proxy: {
