fc1f7dcdc04a3a5ad58c5cae90476675c52ac8b7
Nelson <nelsonba787@gmail.com>
2024-10-29 20:12:22 -0400

    Refactor asset URLs and paths in index.html and vite.config.js

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
   base: "/wrkr-web/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./node_modules", import.meta.url)),
+    },
+  },
   server: {
     proxy: {
       "/api": {
