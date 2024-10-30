c80e659c1a6bef37b42c0f4ca48b24bf25e810b0
Nelson <nelsonba787@gmail.com>
2024-10-29 22:06:47 -0400

    Remove base path from vite.config.js

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/wrkr-web/",
   plugins: [react()],
   server: {
     proxy: {
